089861673828b6d20f8b55b273f14bc2eca35f1e
Kiran Thirumalaichery Dhakshinamoorthy <kirandhakshina@gmail.com>
2019-09-28 17:56:27 -0500

    update new store structure

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1e191e9fb7ffe6fff879";
+/******/ 	var hotCurrentHash = "e094d619489945247049";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
